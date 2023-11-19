00d277d8ede1b01069f9168364a974fe21d9f4f9
Levi G <bobrobot@zoho.com>
2023-11-18 19:54:49 -0800

    Added duolingo

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Client } = require('discord.js-selfbot-v13');
+const { Client, ClientUser } = require('discord.js-selfbot-v13');
 const { ActivityType } = require('discord.js');
 var moment = require('moment-timezone');
 const { trimEnd } = require('lodash');
+const fetch = require('node-fetch');
 const client = new Client({checkUpdate: false}); // All partials are loaded automatically
 var ready = false;
 
@@ -29,14 +30,12 @@ client.on('ready', async () => {
 //on edit
 client.on('messageUpdate', async (oldMessage, newMessage) => {
   try{
-    if (oldMessage.author.id === '347866765608484864' || oldMessage.author.id === '1039281251716182036' || oldMessage.author.id === '533872282393903105') {
-        if(oldMessage.channel.type === 'DM') {
-            time = moment().tz("America/Los_Angeles").format('LT')
-            //send in a channel in a different server
-            client.channels.cache.get('1049207307663323177').send(`**${oldMessage.author.username}** edited a message in **${oldMessage.channel.recipient.username}**'s DMs at **${time}**\n`)
-            client.channels.cache.get('1049207307663323177').send(oldMessage.content)
-            client.channels.cache.get('1049207307663323177').send(newMessage.content)
-        }
+    if(oldMessage.channel.type === 'DM') {
+        time = moment().tz("America/Los_Angeles").format('LT')
+        //send in a channel in a different server
+        client.channels.cache.get('1049207307663323177').send(`**${oldMessage.author.username}** edited a message in **${oldMessage.channel.recipient.username}**'s DMs at **${time}**\n`)
+        client.channels.cache.get('1049207307663323177').send(oldMessage.content)
+        client.channels.cache.get('1049207307663323177').send(newMessage.content)
     }
   } catch (err) {
     console.log(oldMessage.content)
@@ -49,14 +48,12 @@ client.on('messageUpdate', async (oldMessage, newMessage) => {
 //on message delete
 client.on('messageDelete', async (message) => {
   try{
-    if (message.author.id === '347866765608484864' || message.author.id === '1039281251716182036' || message.author.id === '533872282393903105') {
-        if(message.channel.type === 'DM') {
-            time = moment().tz("America/Los_Angeles").format('LT')
-            //send in a channel in a different server
-            client.channels.cache.get('1049207307663323177').send(`**${message.author.username}** deleted a message in **${message.channel.recipient.username}**'s DMs at **${time}**\n`)
-            client.channels.cache.get('1049207307663323177').send(message.content)
+    if(message.channel.type === 'DM') {
+        time = moment().tz("America/Los_Angeles").format('LT')
+        //send in a channel in a different server
+        client.channels.cache.get('1049207307663323177').send(`**${message.author.username}** deleted a message in **${message.channel.recipient.username}**'s DMs at **${time}**\n`)
+        client.channels.cache.get('1049207307663323177').send(message.content)
         }
-    }
   } catch (err) {
     console.log(message.content)
   }
@@ -91,6 +88,29 @@ setInterval(function() {
   if ( date.getSeconds() === 0 || date.getSeconds() === 15 || date.getSeconds() === 30 || date.getSeconds() === 45) {
     client.user.setStatus('online');
     setStatus();
+    currentbio = client.user.bio
+    if (currentbio.toLowerCase().includes("duolingo")) {
+      // get streak from https://www.duolingo.com/2017-06-30/users?username=9021007xyz&fields=users{streakData{currentStreak{length}}}
+      // {"users":[{"streakData":{"currentStreak":{"length":35}}}]}
+      fetch('https://www.duolingo.com/2017-06-30/users?username=9021007xyz&fields=users{streakData{currentStreak{length}}}')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.users[0].streakData.currentStreak.length)
+        // split bio into lines
+        var lines = currentbio.split('\n');
+        // find line containing duolingo
+        for (var i = 0; i < lines.length; i++) {
+          if (lines[i].toLowerCase().includes("duolingo")) {
+            // replace line with new streak
+            lines[i] = "Duolingo Streak: " + data.users[0].streakData.currentStreak.length
+          }
+        }
+        // join lines back together
+        var newbio = lines.join('\n');
+        // set bio
+        client.user.edit({bio: newbio})
+      })
+    }
   }
 }
 }, 1000);
